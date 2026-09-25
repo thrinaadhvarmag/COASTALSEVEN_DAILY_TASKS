363fd336a431ee3a131fc2301bfdd56272196caf
thrinaadhvarmag <thrinaadhCS10-G7@coastalseven.com>
2026-09-25 18:13:28 +0530

    Add reports router to main API file

--- Day8/api/main.py
+++ Day8/api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 
-from api.routers import projects, tasks, auth, dashboard
+from api.routers import projects, tasks, auth, dashboard, reports
 from api.exception_handlers import validation_exception_handler
-
 
 app = FastAPI(
     title="Task Management API",
@@ -11,29 +10,18 @@ app = FastAPI(
     version="1.0.0"
 )
 
-
 app.add_exception_handler(
     RequestValidationError,
     validation_exception_handler
 )
 
-
-# ---------------------------------------------------------
-# ROUTERS
-# ---------------------------------------------------------
-
 app.include_router(projects.router)
 app.include_router(tasks.router)
 app.include_router(auth.router)
 app.include_router(dashboard.router)
+app.include_router(reports.router)
 
-
-# ---------------------------------------------------------
-# ROOT
-# ---------------------------------------------------------
 
 @app.get("/")
 def root():
-    return {
-        "message": "Task Management API is running"
-    }
+    return {"message": "Task Management API is running"}
